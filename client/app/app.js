94047f31ac9712f9b191eb3fc056eeda58622e58
prakal <kalvapal@ualberta.ca>
2015-04-22 21:09:14 -0700

    made changes in authorization to view links

--- client/app/app.js
+++ client/app/app.js
@@ -59,7 +59,8 @@ angular.module('shortly', [
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-    if (!next){
+    // make sure user is authorized before sending to links
+    if (!next && Auth.isAuth()){
       $location.path('/links');
     } else {
       if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
