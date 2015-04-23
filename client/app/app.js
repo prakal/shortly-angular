89ec1c68588d057debe338efa8b9e5226c487b42
prakal <kalvapal@ualberta.ca>
2015-04-22 19:31:54 -0700

    fixed bcrypt issue, and made progress in shortening links

--- client/app/app.js
+++ client/app/app.js
@@ -24,8 +24,8 @@ angular.module('shortly', [
       controller: 'ShortenController'
     })
     .when('/',{
-      templateUrl: 'app/auth/signin.html',
-      controller: 'AuthController'
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
     })
     // Your code here
 
@@ -59,8 +59,12 @@ angular.module('shortly', [
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-      $location.path('/signin');
+    if (!next){
+      $location.path('/links');
+    } else {
+      if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+        $location.path('/signin');
+      }
     }
   });
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,6 +3,7 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links, $http) {
   // Your code here
   $scope.link = {};
+  console.log($scope.text);
   $scope.addLink = function(){
     $http.post('/api/links').
       success(function(data){
